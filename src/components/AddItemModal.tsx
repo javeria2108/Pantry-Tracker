7c403a6166275a11c8884beccad1e30452c32eab
javeria2108 <javeriazh@gmail.com>
2024-08-04 16:51:34 +0500

    bug fix

--- src/components/AddItemModal.tsx
+++ src/components/AddItemModal.tsx
@@ -9,7 +9,7 @@ interface AddItemModalProps {
 
 const AddItemModal: React.FC<AddItemModalProps> = ({ open, handleClose, handleSave }: AddItemModalProps) => {
   const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState<number | ''>(0);
+  const [quantity, setQuantity] = useState<number>(0);
 
   const handleSubmit = () => {
     if (name && quantity !== '') {
