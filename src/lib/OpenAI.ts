ff952e54b4f81e03fd41b60935ee78b66f25da55
javeria2108 <javeriazh@gmail.com>
2024-08-04 21:05:18 +0500

    .

--- src/lib/OpenAI.ts
+++ src/lib/OpenAI.ts
@@ -1,26 +1,36 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import {OpenAI } from "openai"
-const openAI=new OpenAI()
+import { OpenAI } from 'openai';
+const openAI = new OpenAI();
 
-const response=openAI.chat.completions.create({
-    model: "gpt-4-vision-preview",
-    messages:[
+export const getItemNameFromImage = async (imageUrl: string) => {
+  try {
+    const response = await openAI.chat.completions.create({
+      model: 'gpt-4-vision-preview',
+      messages: [
         {
-            role: "user",
-            content:[
-                {
-                    type:"text",
-                    text:"what is the name of item in this image"
-                },{
-                    type:"image_url",
-                    image_url:{
-                        url:"",
-                        detail:"low"
-                    }
-                }
-            ]
-        }
-    ]
-})
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: 'what is the name of item in this image',
+            },
+            {
+              type: 'image_url',
+              image_url: {
+                url: imageUrl,
+                detail: 'low',
+              },
+            },
+          ],
+        },
+      ],
+    });
+    const itemName = response.choices[0];
+    return itemName || '';
+  } catch (error) {
+    console.error('Error fetching item name:', error);
+    return '';
+  }
+};
